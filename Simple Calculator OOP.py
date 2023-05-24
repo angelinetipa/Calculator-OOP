0737c656a0c06922d94ec6685b2a98634979afae
angelinetipa <angelinetipa@gmail.com>
2023-05-24 19:27:43 +0800

    call ask operation

--- Simple Calculator OOP.py
+++ Simple Calculator OOP.py
@@ -1,4 +1,13 @@
+# import userinterface and calculator
+from user_interface import UserInterface
+from simple_calculator import SimpleCalculator
+
+ui = UserInterface()
+calc = SimpleCalculator()
+
 # input for math operation
+operation = ui.ask_operation()
+
 # input for two numbers
 # if math operation is addtion, output sum of two numbers
 # if math operation is subtraction, output difference of two numbers
