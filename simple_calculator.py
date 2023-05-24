52ed74a4b401f2224de2342895f692cb3ae25295
angelinetipa <angelinetipa@gmail.com>
2023-05-24 16:51:18 +0800

    def subtract

--- simple_calculator.py
+++ simple_calculator.py
@@ -4,6 +4,8 @@ class Simple_Calculator:
     # def add
     def add(self, num1, num2):
         return f"{num1} + {num2} = ", float(num1) + float(num2)
-# def subtract
+    # def subtract
+    def subtract(self, num1, num2):
+        return f"{num1} - {num2} = ", float(num1) - float(num2)
 # def multiply
 # def divide
