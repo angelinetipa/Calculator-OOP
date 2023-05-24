95a2a2f3360a163c20669ed4c2ec25a0b5b59324
angelinetipa <angelinetipa@gmail.com>
2023-05-24 16:17:50 +0800

    pseudocode

---
+++ Simple Calculator OOP.py
@@ -0,0 +1,7 @@
+# input for math operation
+# input for two numbers
+# if math operation is addtion, output sum of two numbers
+# if math operation is subtraction, output difference of two numbers
+# if math operation is multiplication, output product of two numbers
+# if math operation is division, output quotient of two numbers
+# input for try again
