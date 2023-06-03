c0709228154314d4d460df856da15b5fb3e80946
angelinetipa <angelinetipa@gmail.com>
2023-06-03 23:14:27 +0800

    added def exponent

--- calculator_tipa.py
+++ calculator_tipa.py
@@ -4,3 +4,7 @@ class CalculatorTipa(SimpleCalculator):
     def remainder_ofdiv(self, num1, num2):
         remainder = num1 % num2
         return remainder
+    
+    def exponent(self, num1, num2):
+        exponential_power = num1 ** num2
+        return exponential_power
