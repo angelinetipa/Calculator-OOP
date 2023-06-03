84390eeb5000ed7d020b82840cc16ebb236c4601
angelinetipa <angelinetipa@gmail.com>
2023-06-03 23:08:10 +0800

    changed name

--- calculator_tipa.py
+++ calculator_tipa.py
@@ -1,6 +1,6 @@
 from simple_calculator import SimpleCalculator
 
 class CalculatorTipa(SimpleCalculator):
-    def divide2(self, num1, num2):
-        remainder_ofquotient = num1 % num2
-        return remainder_ofquotient
+    def remainder_ofdiv(self, num1, num2):
+        remainder = num1 % num2
+        return remainder
