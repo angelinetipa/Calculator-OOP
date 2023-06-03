cd910e968dddbdbdfbc9c2953a1b5a31bb049045
angelinetipa <angelinetipa@gmail.com>
2023-06-03 23:06:08 +0800

    added def divide2

---
+++ calculator_tipa.py
@@ -0,0 +1,6 @@
+from simple_calculator import SimpleCalculator
+
+class CalculatorTipa(SimpleCalculator):
+    def divide2(self, num1, num2):
+        remainder_ofquotient = num1 % num2
+        return remainder_ofquotient
