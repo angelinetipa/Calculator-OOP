55b16c3a7976c89254a9c232fa7533dcbd36699b
angelinetipa <angelinetipa@gmail.com>
2023-06-03 22:12:39 +0800

    changed simple calc oop

--- Simple Calculator OOP.py
+++ Simple Calculator OOP.py
@@ -8,28 +8,31 @@ calc = SimpleCalculator()
 try_again = ""
 while try_again != "no":
     # input for math operation
-    operation = ui.ask_operation()
+    operation = ui.ask_user_operation()
 
     # input for two numbers
-    num1 = ui.ask_num()
-    num2 = ui.ask_num()
+    num1 = ui.ask_user_number()
+    num2 = ui.ask_user_number()
 
     # if math operation is addtion, output sum of two numbers
     if operation == "1":
-        calc.add(num1, num2)
+        answer = calc.add(num1, num2)
+        ui.display_sum(num1, num2, answer)
 
     # if math operation is subtraction, output difference of two numbers
     if operation == "2":
-        calc.subtract(num1, num2)
+        answer = calc.subtract(num1, num2)
+        ui.display_difference(num1, num2, answer)
 
     # if math operation is multiplication, output product of two numbers
     if operation == "3":
-        calc.multiply(num1, num2)
-
+        answer = calc.multiply(num1, num2)
+        ui.display_product(num1, num2, answer)
     # if math operation is division, output quotient of two numbers
     if operation == "4":
-        calc.divide(num1, num2)
-
+        answer = calc.divide(num1, num2)
+        ui.display_quotient(num1, num2, answer)
+        
     # input for try again
     try_again = input("Do you want to try again (yes/no)?😁  ").strip().lower()
 
